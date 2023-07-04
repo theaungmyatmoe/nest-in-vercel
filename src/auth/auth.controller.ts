ab306cd976ed8d4371a852e394d0212800a58962
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-05 00:06:59 +0630

    feat: add nest js validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signup() {
+  async signup(@Body() authDto: AuthDto) {
+    console.log(authDto);
     return this.authService.signup();
   }
 

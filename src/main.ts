73f32146e1045d4cd9318738c60bcaaac26dea5e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-06 02:05:52 +0630

    feat: add vercel

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true, // allow cors
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove all properties that are not in the DTO
     }),
   );
-  await app.listen(3000);
+  await app.listen(5019);
 }
 
 bootstrap();
